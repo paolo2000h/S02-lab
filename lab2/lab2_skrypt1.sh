a85f74e8c8e18ba57d029b145b63d2d03b7a709d
paolo2000h <paoloplayer@gmail.com>
2022-03-21 15:27:58 +0100

    added script 1 and readme

---
+++ lab2/lab2_skrypt1.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+# W zadanym katalogu przerobić wszystkie dowiązania twarde wskazujące 
+# na pliki regularne w tym katalogu – na dowiązania symboliczne 
+# (trzeba pozostawić jeden plik/dowiązanie, na który wskażą pozostałe).
+if [ $# != 1 ]
+then
+echo Podaj nazwe katalogu
+exit 0
+elif [ ! -d $1 ]
+then
+echo Podaj nazwe katalogu
+exit 0
+fi
+
+for file in $1/*
+do
+    if [ ! -h $file ]
+    then
+    for file1 in $1/*
+    do
+        if [ ! -h $file1 ]
+        then
+        if [ $file != $file1 ]
+        then
+            if [ $file -ef $file1 ] 
+            then 
+            rm $file
+            ln -s $file1 $file
+            fi
+        fi
+        fi
+    done
+    fi
+done
