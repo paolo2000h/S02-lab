0d58827c443ccd154d2b5642e1f81ea5ffefeadd
paolo2000h <paoloplayer@gmail.com>
2022-06-13 12:24:49 +0200

    Added lab7

---
+++ lab7/skrypt_1.py
@@ -0,0 +1,30 @@
+# w zadanym pliku tekstowym zawierajacym program cpp 
+# wylistowac teksty komentrzy zaznaczonych przez //
+
+#!/usr/bin/env python3
+from nis import match
+import os
+import sys
+import re
+
+if len(sys.argv) != 2:
+    print("Podaj plik cpp")
+    sys.exit()
+
+if not os.path.exists(sys.argv[1]):
+    print("Podaj plik cpp")
+    sys.exit()
+
+with open (sys.argv[1], 'r') as cpp_file:
+    text = cpp_file.read()
+
+    pattern = re.compile('(?://(.*?)\n)' ,re.S)
+    matches = pattern.findall(text)
+    for match in matches:
+        print(match)
+
+sys.exit()
+    
+
+
+
